04227369c77a17be57396172bf9352012f1d390e
jia ge <121222876+Greellow@users.noreply.github.com>
2025-07-23 22:26:24 +0800

    套餐分页

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,9 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface SetmealService {
     void saveWithDish(SetmealDTO dto);
+
+    PageResult pageQuery(SetmealPageQueryDTO dto);
 }
